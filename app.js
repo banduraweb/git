e00d069677d4177e1ef036a3ce81566f2c1a5232
banduraweb <abandu@softserveinc.com>
2022-07-14 21:20:09 +0300

    qwerty23

--- app.js
+++ app.js
@@ -2,4 +2,6 @@ const _ = (x,y) => x+y
 console.log('hello merge main old');
 console.log('here const f');
 const qwerty = ()=> Math.random()
+const qwerty2 = ()=> Math.random()
+const qwerty23 = ()=> Math.random()
 // restore --stage
