ead856f6edf7af12c6938ffafb3f469eeae89d8d
banduraweb <abandu@softserveinc.com>
2022-08-13 14:41:16 +0300

    revert2

--- app.js
+++ app.js
@@ -2,6 +2,4 @@ const _ = (x,y) => x+y
 console.log('hello merge main old');
 console.log('here const f');
 const qwerty = ()=> Math.random()
-const qwerty2 = ()=> Math.random()
-const qwerty23 = ()=> Math.random()
 // restore --stage
